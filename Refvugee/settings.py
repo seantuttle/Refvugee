72aa37113cd4ed17255a11105056bc126c7a5ae0
tuttlsj1 <sean.j.tuttle@vanderbilt.edu>
2020-09-02 22:41:11 -0500

    Initial Commit

--- Refvugee/settings.py
+++ Refvugee/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'm1v+n1v(5sr3uorau20wxv#a#v(l6(f=llzzhcnm6q_9qae0v)'
+SECRET_KEY = os.environ.get('REFVUGEE_SECURITY_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
